50690db1a778d75b604bd09a6a9e819de3399a07
Lucas Oliveira <olivmath@protonmail.com>
2024-10-29 17:40:49 -0300

    update mod2 aula2

---
+++ web3-libs/mod2/playground/aula3/getTransaction.js
@@ -0,0 +1,25 @@
+// 1. Instale e importe a biblioteca Web3.js
+import Web3 from "web3";
+import readline from "readline";
+
+// 2. Configure o provider
+const RPC_URL = "http://127.0.0.1:8545";
+const web3 = new Web3(RPC_URL);
+
+// 3. Obtenha o endereço da conta via readline input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Digite o endereço da conta: ", (txHash) => {
+  web3.eth
+    .getTransaction(txHash)
+    .then((transaction) => {
+      console.log(`Transação de ${txHash}`, transaction);
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar a transação:", error);
+    });
+  rl.close();
+});
