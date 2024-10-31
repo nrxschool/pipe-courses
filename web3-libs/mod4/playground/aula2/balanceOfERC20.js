3d640f65d7ff54d7fb4006a9bdf1fa3497d39c15
Lucas Oliveira <olivmath@protonmail.com>
2024-10-31 19:13:48 -0300

    update: mod 4 aula 2

--- web3-libs/mod4/playground/aula2/balanceOfERC20.js
+++ web3-libs/mod4/playground/aula2/balanceOfERC20.js
@@ -1,10 +1,10 @@
-// 1. Instale e importe a biblioteca Web3.js
-import Web3 from "web3";
+// 1. Instale e importe a biblioteca Ethers.js
+import { ethers } from "ethers";
 import readline from "readline";
 
 // 2. Configure o provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const provider = new ethers.JsonRpcProvider(RPC_URL);
 const daiAbi = [
   {
     constant: true,
@@ -37,16 +37,14 @@ const askForAccountAddress = (contractAddress) => {
 
 // Função para verificar o saldo
 const checkBalance = (contractAddress, accountAddress) => {
-  const daiContract = new web3.eth.Contract(daiAbi, contractAddress);
+  const daiContract = new ethers.Contract(contractAddress, daiAbi, provider);
 
-  daiContract.methods
-    .balanceOf(accountAddress)
-    .call()
+  daiContract.balanceOf(accountAddress)
     .then((balance) => {
-      console.log("Saldo do Token:", web3.utils.fromWei(balance, "ether"));
+      console.log("Saldo do Token:", ethers.formatEther(balance));
     })
     .catch((error) => {
-      console.error("Erro ao consultar o saldo de DAI:", error);
+      console.error("Erro ao consultar o saldo:", error);
     })
     .finally(() => {
       rl.close();
--- web3-libs/mod4/playground/aula2/convertWei.js
+++ web3-libs/mod4/playground/aula2/convertWei.js
@@ -1,10 +1,10 @@
-// 1. Instale e importe a biblioteca Web3.js
-import Web3 from "web3";
+// 1. Instale e importe a biblioteca Ethers.js
+import { ethers } from "ethers";
 import readline from "readline";
 
 // 2. Configure o provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const provider = new ethers.JsonRpcProvider(RPC_URL);
 
 // 3. Obtenha o endereço da conta via readline input
 const rl = readline.createInterface({
@@ -13,11 +13,10 @@ const rl = readline.createInterface({
 });
 
 rl.question("Digite o endereço da conta: ", (address) => {
-  web3.eth
-    .getBalance(address)
+  provider.getBalance(address)
     .then((balance) => {
-      const etherBalance = web3.utils.fromWei(balance, "ether");
-      console.log("Saldo em wei:", balance);
+      const etherBalance = ethers.formatEther(balance);
+      console.log("Saldo em wei:", balance.toString());
       console.log("Saldo em ether:", etherBalance);
     })
     .catch((error) => {
