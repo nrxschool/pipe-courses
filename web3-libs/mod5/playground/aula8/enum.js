05781dcf033f2d8d226c146d940a98e491fa9cda
Lucas Oliveira <olivmath@protonmail.com>
2024-10-31 17:06:36 -0300

    aula 8 viem

--- web3-libs/mod5/playground/aula8/enum.js
+++ web3-libs/mod5/playground/aula8/enum.js
@@ -1,24 +1,37 @@
-import { Web3 } from "web3";
+import { createPublicClient, createWalletClient, http } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { foundry } from 'viem/chains';
 import abi from "./abi.js";
 
 // Configuração do provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const transport = http(RPC_URL);
+
+const publicClient = createPublicClient({
+  chain: foundry,
+  transport
+});
+
+const walletClient = createWalletClient({
+  chain: foundry,
+  transport
+});
 
 const PRIVATE_KEY =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const account = web3.eth.accounts.wallet.add(PRIVATE_KEY)[0];
+const account = privateKeyToAccount(PRIVATE_KEY);
 
 // Endereço do contrato
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-// Cria uma instância do contrato
-const contract = new web3.eth.Contract(abi, contractAddress);
-
-
 // Nova função para chamar pushGen
 async function getNamesByGender(gender) {
-  const names = await contract.methods.pushGen(gender).call();
+  const names = await publicClient.readContract({
+    address: contractAddress,
+    abi: abi,
+    functionName: 'pushGen',
+    args: [gender]
+  });
   console.log(names);
 }
 
--- web3-libs/mod5/playground/aula8/structs.js
+++ web3-libs/mod5/playground/aula8/structs.js
@@ -1,19 +1,27 @@
-import { Web3 } from "web3";
+import { createPublicClient, createWalletClient, http } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { foundry } from 'viem/chains';
 import abi from "./abi.js";
 
 // Configuração do provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const transport = http(RPC_URL);
 
-const PRIVATE_KEY =
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const account = web3.eth.accounts.wallet.add(PRIVATE_KEY)[0];
+const publicClient = createPublicClient({
+  chain: foundry,
+  transport
+});
+
+const walletClient = createWalletClient({
+  chain: foundry,
+  transport
+});
+
+const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const account = privateKeyToAccount(PRIVATE_KEY);
 
 // Endereço do contrato
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
-// Cria uma instância do contrato
-const contract = new web3.eth.Contract(abi, contractAddress);
 
 async function addPeople(name, age, gender) {
   const person = {
@@ -22,7 +30,15 @@ async function addPeople(name, age, gender) {
     gender: gender,
   };
 
-  await contract.methods.pushPeople(person).send({ from: account.address });
+  const { request } = await publicClient.simulateContract({
+    address: contractAddress,
+    abi: abi,
+    functionName: 'pushPeople',
+    args: [person],
+    account
+  });
+
+  await walletClient.writeContract(request);
 }
 
 const male = 0;
