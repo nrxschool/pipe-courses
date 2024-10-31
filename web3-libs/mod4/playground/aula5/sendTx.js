ac11230df39a47e0db153419ea0e7a4c0ae8c5fd
Clarice Hatischvili <claricehatischvili@gmail.com>
2024-10-31 19:50:55 -0300

    Aula 5

--- web3-libs/mod4/playground/aula5/sendTx.js
+++ web3-libs/mod4/playground/aula5/sendTx.js
@@ -1,57 +1,51 @@
-import { Web3 } from "web3";
+import { ethers } from "ethers";
 
 // Configuração do provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const provider = new ethers.JsonRpcProvider(RPC_URL);
 
 // Configuração do signer
 const PRIVATE_KEY =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const account = web3.eth.accounts.wallet.add(PRIVATE_KEY)[0];
+const wallet = new ethers.Wallet(PRIVATE_KEY).connect(provider);
 
 // Função para enviar uma transação
-async function sendTransaction(account) {
+async function sendTransaction(wallet) {
   try {
-    // Obtém o nonce do endereço
-    const nonce = await web3.eth.getTransactionCount(account.address);
+    // Obtém o nonce do endereço usando o provider
+    const nonce = await provider.getTransactionCount(wallet.address);
 
     // Define o destinatário e o valor da transação
     const toAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
     const valueInEther = "1";
 
     // Obtém o preço atual do gás da rede
-    const gasPrice = await web3.eth.getGasPrice();
+    const feeData = await provider.getFeeData(); // Obtém os dados da taxa
+    const gasPrice = feeData.gasPrice; // Extraindo o gasPrice do feeData
 
     // Calcula o gás necessário para a transação
-    const gasLimit = await web3.eth.estimateGas({
+    const gasLimit = await provider.estimateGas({
       to: toAddress,
-      from: account.address,
-      value: web3.utils.toWei(valueInEther, "ether"),
+      from: wallet.address,
+      value: ethers.parseEther(valueInEther),
     });
 
     // Monta a transação
     const transaction = {
       to: toAddress,
-      value: web3.utils.toWei(valueInEther, "ether"),
-      gas: gasLimit,
+      value: ethers.parseEther(valueInEther),
+      gasLimit: gasLimit,
       gasPrice: gasPrice,
       nonce: nonce,
     };
 
-    // Assina a transação
-    const signedTransaction = await web3.eth.accounts.signTransaction(
-      transaction,
-      PRIVATE_KEY
-    );
-
     // Envia a transação
-    const receipt = await web3.eth.sendSignedTransaction(
-      signedTransaction.rawTransaction
-    );
+    const txResponse = await wallet.sendTransaction(transaction);
+    const receipt = await txResponse.wait();
 
     console.log(
       "Transação enviada com sucesso! Hash:",
-      receipt.transactionHash
+      txResponse.hash // Alterado para acessar o hash diretamente do txResponse
     );
   } catch (error) {
     console.error("Erro ao enviar a transação:", error);
@@ -59,4 +53,4 @@ async function sendTransaction(account) {
 }
 
 // Chama a função para enviar a transação
-sendTransaction(account);
+sendTransaction(wallet);
