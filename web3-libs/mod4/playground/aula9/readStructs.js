4cdf36d352eaf655d496dded0e9f40faf7e165ae
Lucas Oliveira <olivmath@protonmail.com>
2024-10-31 17:29:40 -0300

    mod 4 aula 9

--- web3-libs/mod4/playground/aula9/readStructs.js
+++ web3-libs/mod4/playground/aula9/readStructs.js
@@ -1,22 +1,22 @@
-import { Web3 } from "web3";
+import { ethers } from "ethers";
 import abi from "../aula8/abi.js";
 
 // Configuração do provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const provider = new ethers.JsonRpcProvider(RPC_URL);
 
 const PRIVATE_KEY =
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const account = web3.eth.accounts.wallet.add(PRIVATE_KEY)[0];
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
 // Endereço do contrato
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // Cria uma instância do contrato
-const contract = new web3.eth.Contract(abi, contractAddress);
+const contract = new ethers.Contract(contractAddress, abi, wallet);
 
 async function getPeople() {
-  const person = await contract.methods.getPerson().call();
+  const person = await contract.getPerson();
   console.log("Nome:", person.name);
   console.log("Idade:", person.age);
   console.log("Gênero:", person.gender);
