6f081075ceeff54be2bb46e4c88721f1bbcc26c5
Clarice Hatischvili <claricehatischvili@gmail.com>
2024-10-31 19:24:05 -0300

    Aula 3 ethers

--- web3-libs/mod2/playground/aula3/getTransaction.js
+++ web3-libs/mod2/playground/aula3/getTransaction.js
@@ -1,10 +1,10 @@
-// 1. Instale e importe a biblioteca Web3.js
-import Web3 from "web3";
+// 1. Instale e importe a biblioteca Ethers.js
+import { ethers } from "ethers";
 import readline from "readline";
 
 // 2. Configure o provider
 const RPC_URL = "http://127.0.0.1:8545";
-const web3 = new Web3(RPC_URL);
+const provider = new ethers.JsonRpcProvider(RPC_URL);
 
 // 3. Obtenha o endereço da conta via readline input
 const rl = readline.createInterface({
@@ -12,14 +12,12 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Digite o hash da transação: ", (txHash) => {
-  web3.eth
-    .getTransaction(txHash)
-    .then((transaction) => {
-      console.log(`Transação de ${txHash}`, transaction);
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar a transação:", error);
-    });
+rl.question("Digite o hash da transação: ", async (txHash) => {
+  try {
+    const transaction = await provider.getTransaction(txHash);
+    console.log(`Transação de ${txHash}`, transaction);
+  } catch (error) {
+    console.error("Erro ao buscar a transação:", error);
+  }
   rl.close();
 });
