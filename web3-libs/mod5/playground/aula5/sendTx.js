d79a34a4c18ff599fced1281f1690159e1352212
Lucas Oliveira <olivmath@protonmail.com>
2024-11-01 17:56:17 -0300

    aula 5

--- web3-libs/mod5/playground/aula5/sendTx.js
+++ web3-libs/mod5/playground/aula5/sendTx.js
@@ -1,37 +1,34 @@
-import { createPublicClient, createWalletClient, http, parseEther } from 'viem';
-import { privateKeyToAccount } from 'viem/accounts';
+import { createWalletClient, http, parseEther } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { anvil } from "viem/chains";
 
-// Configuração do provider para leitura (cliente público)
-const publicClient = createPublicClient({
-  transport: http('http://127.0.0.1:8545'),
-});
-
-// Configuração do provider e do signer para transações (cliente com carteira)
 const PRIVATE_KEY =
-  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-const walletClient = createWalletClient({
-  transport: http('http://127.0.0.1:8545'),
-});
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
 const account = privateKeyToAccount(PRIVATE_KEY);
 
+const signer = createWalletClient({
+  chain: anvil,
+  transport: http(),
+  account,
+});
+
 // Função para enviar uma transação usando o cliente da carteira
-async function sendTransaction(account) {
+async function sendTransaction() {
   try {
-    const toAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
-    const valueInEther = '1';
+    const toAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    const valueInEther = "1";
 
     // Envia a transação
-    const hash = await walletClient.sendTransaction({
-      account,
+    const hash = await signer.sendTransaction({
       to: toAddress,
       value: parseEther(valueInEther),
     });
 
-    console.log('Transação enviada com sucesso! Hash:', hash);
+    console.log("Transação enviada com sucesso! Hash:", hash);
   } catch (error) {
-    console.error('Erro ao enviar a transação:', error);
+    console.error("Erro ao enviar a transação:", error);
   }
 }
 
-// Chama a função para enviar a transação
-sendTransaction(account);
+sendTransaction();
